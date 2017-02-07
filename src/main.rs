7f17b1fb442aa24fec74898319353e6febbf00d8
Masato Hori <masato@fixstars.com>
2017-02-07 16:23:50 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+extern crate time;
+
+fn main() {
+    let size = 4*1024*1024;
+    let mut in_buf = Vec::<f32>::with_capacity(size);
+    let mut out_buf = Vec::<f32>::with_capacity(size);
+    in_buf.resize(size, 0.0f32);
+    out_buf.resize(size, 0.0f32);
+    let iter_num = 10;
+    for _ in 0..iter_num {
+        for i in 0..size {
+            in_buf[i] = rand::random::<f32>();
+        }
+        let start = time::precise_time_ns();
+        out_buf.copy_from_slice(&in_buf);
+        let end = time::precise_time_ns();
+        let dur = end - start;
+        let msec = (dur as f32) * 0.001 * 0.001; // ns to ms
+        println!("time: {:.2} ms", msec);
+    }
+}

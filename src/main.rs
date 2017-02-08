e662c0b8726e7156663d4a006d1ee0324cf2ebcf
Masato Hori <rillomas@gmail.com>
2017-02-08 14:11:38 +0900

    added error checking and return code if errors

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate time;
 
 fn main() {
     let size = 4*1024*1024;
-    let mut in_buf = Vec::<f32>::with_capacity(size);
-    let mut out_buf = Vec::<f32>::with_capacity(size);
+    let mut in_buf = Vec::<f32>::new();
+    let mut out_buf = Vec::<f32>::new();
     in_buf.resize(size, 0.0f32);
     out_buf.resize(size, 0.0f32);
     let iter_num = 10;
@@ -18,5 +18,14 @@ fn main() {
         let dur = end - start;
         let msec = (dur as f32) * 0.001 * 0.001; // ns to ms
         println!("time: {:.2} ms", msec);
+        for i in 0..size {
+            let exp = in_buf[i];
+            let act = out_buf[i];
+            if exp != act {
+                println!("Results mismatch at {}: expected {} actual {}", i, exp, act);
+                std::process::exit(-1) 
+            }
+        }
     }
+    std::process::exit(0) 
 }
